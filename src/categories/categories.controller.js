b60b5d654c60aa08baca045675a29f6dfb4b3a01
smiley_12_2 <joseandresgavilanes2012@gmail.com>
2022-11-19 16:18:07 -0500

    categories services and controller

--- src/categories/categories.controller.js
+++ src/categories/categories.controller.js
@@ -1,4 +1,4 @@
-const Categories = require('../models/categories.models')
+const Categories = require("../models/categories.models");
 
 //? Ver todas las categorias
 //? Ver una categoria en especifico
@@ -6,38 +6,39 @@ const Categories = require('../models/categories.models')
 //? Eliminar categoria
 
 const getAllCategories = async () => {
-    const data = await Categories.findAll()
-    return data
-}
+  const data = await Categories.findAll();
+  return data;
+};
 
 const getCategoryById = async (id) => {
-    const data = await Categories.findOne({
-        where: {
-            id
-        }
-    })
-    return data
-}
+  const data = await Categories.findOne({
+    where: {
+      id,
+    },
+  });
+  return data;
+};
 
-const createCategory = async (name) => {
-    const data = await Categories.create({
-        name
-    })
-    return data
-}
+const createCategory = async (data) => {
+  const data = await Categories.create({
+    name: data.name,
+    urlImg: data.urlImg,
+  });
+  return data;
+};
 
 const deleteCategory = async (id) => {
-    const data = await Categories.destroy({
-        where: {
-            id
-        }
-    })
-    return data
-}
+  const data = await Categories.destroy({
+    where: {
+      id,
+    },
+  });
+  return data;
+};
 
 module.exports = {
-    getAllCategories,
-    getCategoryById,
-    createCategory,
-    deleteCategory
-}
+  getAllCategories,
+  getCategoryById,
+  createCategory,
+  deleteCategory,
+};
